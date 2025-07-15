e288fda9b7a3606e76cfb23fd58d3d79663cb404
defendimattia <defendi.mattia92@gmail.com>
2025-07-15 16:14:13 +0200

    Add store method

--- src/main/java/org/lessons/java/crud/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/lessons/java/crud/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -8,10 +8,18 @@ import org.lessons.java.crud.spring_la_mia_pizzeria_crud.repository.PizzaReposit
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 @RequestMapping("/pizzas")
@@ -59,6 +67,18 @@ public class PizzaController {
 
         return "pizzas/create";
     }
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult, Model model) {
+
+        if(bindingResult.hasErrors()) {
+            return "/pizzas/create";
+        }
+
+        repository.save(formPizza);
+        return "redirect:/pizzas";
+    }
+    
     
 
 }
