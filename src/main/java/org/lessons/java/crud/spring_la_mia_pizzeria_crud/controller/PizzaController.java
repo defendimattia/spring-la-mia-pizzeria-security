52b4e7b2316a8daefbf787263e49c06bf409803b
defendimattia <defendi.mattia92@gmail.com>
2025-07-16 10:54:06 +0200

    Add edit and update method with relative html page

--- src/main/java/org/lessons/java/crud/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/lessons/java/crud/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequestMapping("/pizzas")
@@ -45,7 +45,7 @@ public class PizzaController {
 
             pizzas = filteredPizzas;
         }
-        
+
         model.addAttribute("pizzas", pizzas);
         return "pizzas/index";
     }
@@ -70,14 +70,32 @@ public class PizzaController {
     @PostMapping("/create")
     public String store(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult, Model model) {
 
-        if(bindingResult.hasErrors()) {
+        if (bindingResult.hasErrors()) {
             return "/pizzas/create";
         }
 
         repository.save(formPizza);
         return "redirect:/pizzas";
     }
-    
-    
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+
+        model.addAttribute("pizza", repository.findById(id).get());
+        return "/pizzas/edit";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String update(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult,
+            Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "/pizzas/edit";
+        }
+
+        repository.save(formPizza);
+
+        return "redirect:/pizzas";
+    }
 
 }
