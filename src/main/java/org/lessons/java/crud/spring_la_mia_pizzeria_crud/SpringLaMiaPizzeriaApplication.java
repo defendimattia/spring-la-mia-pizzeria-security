3718bbbb5855fef4d74e7ac3e28f0d30240c0cb3
defendimattia <defendi.mattia92@gmail.com>
2025-07-28 15:30:38 +0200

    initial commit

--- src/main/java/org/lessons/java/crud/spring_la_mia_pizzeria_crud/SpringLaMiaPizzeriaApplication.java
+++ src/main/java/org/lessons/java/crud/spring_la_mia_pizzeria_crud/SpringLaMiaPizzeriaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringLaMiaPizzeriaCrudApplication {
+public class SpringLaMiaPizzeriaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringLaMiaPizzeriaCrudApplication.class, args);
+		SpringApplication.run(SpringLaMiaPizzeriaApplication.class, args);
 	}
 
 }
