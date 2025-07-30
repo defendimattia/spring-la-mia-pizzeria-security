966a66321646c87e44f39d945dc85d72f70bba50
defendimattia <defendi.mattia92@gmail.com>
2025-07-30 16:30:46 +0200

    Add edit functionality for special offers and update related templates

--- src/main/java/org/lessons/java/crudrelazioni/spring_la_mia_pizzeria/controller/SpecialOffertController.java
+++ src/main/java/org/lessons/java/crudrelazioni/spring_la_mia_pizzeria/controller/SpecialOffertController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.validation.Valid;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -31,6 +33,27 @@ public class SpecialOffertController {
         repository.save(formSpecialOffert);
 
         return "redirect:/pizzas";
+    }
+
+    @PostMapping("/{id}/edit")
+    public String update(@Valid @ModelAttribute("specialOffert") SpecialOffert formSpecialOffert,
+            BindingResult bindingResult,
+            Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "/special-offerts/create-or-edit";
+        }
+
+        repository.save(formSpecialOffert);
+
+        return "redirect:/pizzas/" + formSpecialOffert.getPizza().getId();
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+
+        model.addAttribute("specialOffert", repository.findById(id).get());
+        return "/special-offerts/create-or-edit";
     }
 
 }
